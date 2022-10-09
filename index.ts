23f7c970e8e719fc9969b594951c82915978dcfa
lisushka <lisushka@users.noreply.github.com>
2022-10-09 22:17:41 +1100

    Draft empty methods

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import * as path from 'path'
 
 export interface Task {
   name: string
@@ -6,22 +7,37 @@ export interface Task {
 }
 
 class ConfigurationParser {
+  readConfig (): void {
 
+  }
+
+  parseConfig (): void{
+
+  }
+
+  executeStep (): void {
+
+  }
+
+  parsePackageOptions (): void {
+    // package install
+    // package update
+    // package delete
+  }
+
+  parseFileOptions (): void {
+    // file create
+    // file update
+    // file delete
+    // note: untie metadata?
+  }
+
+  parseServerOptions (): void {
+    // start/stop/restart server on changes
+    // should this be a separate task?
+  }
 }
 
 const configParser = new ConfigurationParser()
 
 export default configParser
-
-// package install
-// package update
-// package delete
-
-// file create
-// file update
-// file delete
-// note: untie metadata?
-
-// start/stop/restart server on changes
-// should this be a separate task?
-
