dd57bb265a87953dec22992576961f035200dfeb
lisushka <lisushka@users.noreply.github.com>
2022-10-10 07:16:16 +1100

    Updates to expected methods

--- index.ts
+++ index.ts
@@ -8,15 +8,23 @@ export interface Task {
 
 class ConfigurationParser {
   readConfig (): void {
-
+    // read in config file (JSON parse?)
+    // hand off to the global config creator
   }
 
-  parseConfig (): void{
+  createConfig (): void {
+    // for each task in config file, check type
+    // hand off to the correct parser
+  }
 
+  checkCurrentState (): void {
+    // run command which checks current state
+    // if current state does not match desired configuration, hand off to execute
   }
 
   executeStep (): void {
-
+    // execute configuration change
+    // return success or failure
   }
 
   parsePackageOptions (): void {
