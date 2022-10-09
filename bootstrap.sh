68363f07c878c3573aa5aa55214797d2a4d3a135
lisushka <lisushka@users.noreply.github.com>
2022-10-10 07:38:48 +1100

    Update bootstrap file to bootstrap Node

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-curl -OL https://go.dev/dl/go1.19.2.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.2.linux-amd64.tar.gz
-sudo sh -c "echo 'export PATH=\$PATH:/usr/local/go/bin' >> /etc/profile"
-go version
+cd ~
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+node -v
