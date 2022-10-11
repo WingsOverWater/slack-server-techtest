d3d50b6cbf5d51223837563323036ff069c4ab25
Dawn E. Collett <lisushka@users.noreply.github.com>
2022-10-12 09:15:43 +1100

    cleanup after testing tool

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 
 cd ~
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 node -v
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { exec } from 'child_process'
+import { spawn } from 'child_process'
 import * as fs from 'fs'
 import * as path from 'path'
 
@@ -31,10 +31,12 @@ interface FileTask {
   type: string
   args: {
     name: string
+    path?: string
     content?: string
     owner?: string
     group?: string
     perms?: string
+    remove?: boolean
   }
   restart?: {
     service: string
@@ -57,24 +59,23 @@ interface ConfigData {
 async function readConfig (filePath: string): Promise<void> {
   let configData: ConfigData
   try {
-    console.log(`Reading data from ${filePath}`)
+    console.log(`***Reading data from ${filePath}***`)
     configData = JSON.parse(fs.readFileSync(path.join(__dirname, filePath), 'utf8'))
   } catch (error) {
-    throw new Error(`File ${filePath} is missing or malformed.  ${error}`)
+    throw new Error(`File ${filePath} is missing or malformed: ${error}`)
   }
-  // may not be able to differentiate between failure to parse the JSON and an invalid file name?  need to test
   await createConfig(configData)
 }
 
 async function createConfig (configData: ConfigData): Promise<void> {
-  configData.tasks.forEach(async taskData => {
+  for(const taskData of configData.tasks) {
     switch(taskData.type){
       case 'package': {
         try {
           await parsePackageOptions(taskData as PackageTask)
         }
         catch (error) {
-          throw new Error(`Step ${taskData.name} is missing a required argument: ${error}`)
+          throw new Error(`Step ${taskData.name} failed to execute: ${error}`)
         }
         break
       }
@@ -83,7 +84,7 @@ async function createConfig (configData: ConfigData): Promise<void> {
           await parseFileOptions(taskData as FileTask)
         }
         catch (error) {
-          throw new Error(`Step ${taskData.name} is missing a required argument: ${error}`)
+          throw new Error(`Step ${taskData.name} failed to execute: ${error}`)
         }
         break
       }
@@ -92,7 +93,7 @@ async function createConfig (configData: ConfigData): Promise<void> {
           await parseServiceOptions(taskData as ServiceTask)
         }
         catch (error) {
-          throw new Error(`Step ${taskData.name} is missing a required argument: ${error}`)
+          throw new Error(`Step ${taskData.name} failed to execute: ${error}`)
         }
         break
       }
@@ -100,32 +101,16 @@ async function createConfig (configData: ConfigData): Promise<void> {
         throw new Error('Unrecognised task type.  Valid tasks: package, file, service')
       }
     }
-  })
-}
-
-// these two methods should be generalisable, I just can't work out how yet
-async function runCommandWithCheck (changeCommand: string, checkCommand: string, condition: string, restart: {service: string} | undefined): Promise<void> {
-  try {
-    let check = await runShellCommand(checkCommand)
-    if (check == condition) {
-      await runShellCommand(changeCommand)
-      if (restart) {
-        await handleRestartOnTask(restart.service)
-      }
-    }
-  } catch (error) {
-    throw new Error(`Changeset execution failed: ${error}`)
   }
 }
 
-async function runCommandWithNotNullCheck (changeCommand: string, checkCommand: string, restart: {service: string} | undefined): Promise<void> {
+async function runCommandWithCheck (changeCommand: string, changeArgs: string[], condition: boolean): Promise<boolean> {
   try {
-    let check = await runShellCommand(checkCommand)
-    if (check != "") {
-      await runShellCommand(changeCommand)
-      if (restart) {
-        await handleRestartOnTask(restart.service)
-      }
+    if (condition) {
+      await runShellCommand(changeCommand, changeArgs, true)
+      return true
+    } else {
+      return false
     }
   } catch (error) {
     throw new Error(`Changeset execution failed: ${error}`)
@@ -133,58 +118,88 @@ async function runCommandWithNotNullCheck (changeCommand: string, checkCommand: 
 }
 
 async function parsePackageOptions (packageData: PackageTask): Promise<void> {
-  console.log(`Executing task ${packageData.name}...`)
+  console.log(`***Executing task ${packageData.name}...***`)
+  let installed = true
+  let packageChanged = false
+  try {
+    await runShellCommand('dpkg', ['-s', packageData.args.package], false)
+  } catch (error) {
+    installed = false
+  }
   switch(packageData.command){
     case 'install': {
-      await runCommandWithNotNullCheck(`sudo apt update && sudo apt install ${packageData.args.package}`, `apt --installed list | grep ${packageData.args.package}`, packageData.restart)
+      packageChanged = await runCommandWithCheck('sh', ['-c', `sudo apt-get update && sudo apt-get install -y ${packageData.args.package}`], !installed)
       break
     }
     case 'upgrade': {
-      await runCommandWithNotNullCheck(`sudo apt update && sudo apt upgrade ${packageData.args.package}`, `apt --installed list | grep ${packageData.args.package}`, packageData.restart)
+      packageChanged = await runCommandWithCheck('sh', ['-c', `sudo apt-get update && sudo apt-get upgrade -y ${packageData.args.package}`], !installed)
       break
     }
     case 'remove': {
-      await runCommandWithCheck(`sudo apt update && sudo apt remove ${packageData.args.package}`, `apt --installed list | grep ${packageData.args.package}`, "",packageData.restart)
+      packageChanged = await runCommandWithCheck('sh', ['-c', `sudo apt-get update && sudo apt-get remove -y ${packageData.args.package}`], installed)
       break
     }
     default: {
       throw new Error('Unrecognised package command.  Valid commands: install, upgrade, remove')
     }
   }
-
+  if (packageChanged) {
+    if (packageData.restart) {
+      await handleRestartOnTask(packageData.restart.service)
+    }
+  } else {
+    console.log('***Changeset matches current configuration, skipped...***')
+  }
 }
 
 async function parseFileOptions (fileData: FileTask): Promise<void> {
-  console.log(`Executing task ${fileData.name}...`)
-  await runCommandWithCheck(`touch ${fileData.name}`, `find . -type f -name ${fileData.name}`, `./${fileData.name}`, fileData.restart)
-  if (fileData.args.content != undefined) {
-    // need to run a check here
-    await runShellCommand(`printf ${fileData.args.content} > ${fileData.args.name}`)
+  console.log(`***Executing task ${fileData.name}...***`)
+  let fileExists = await runShellCommand('sh', ['-c', `find ${fileData.args.path} -type f -name ${fileData.args.name}`], false)
+  let fileChanged = false
+  if (fileData.args.remove) {
+    fileChanged = await runCommandWithCheck('sh', ['-c', `sudo rm ${fileData.args.path}/${fileData.args.name}`], fileExists.includes(fileData.args.name))
+  } else {
+    fileChanged = await runCommandWithCheck('sh', ['-c', `touch ${fileData.args.path}/${fileData.args.name}`], !(fileExists.includes(fileData.args.name)))
+    if (fileData.args.content != undefined) {
+      let fileContentMatches = await runShellCommand('sh', ['-c', `echo ${fileData.args.path}/${fileData.args.name}`], false) == await runShellCommand('sh', ['-c', `echo ${fileData.args.content}`], false)
+      try {
+        fileChanged = await runCommandWithCheck('sh', ['-c', `echo ${fileData.args.content} > ${fileData.args.path}/${fileData.args.name}`], fileContentMatches)
+      } catch (error) {
+        throw new Error(`Content change failed to execute: ${error}`)
+      }
+    }
+    if (fileData.args.owner != undefined) {
+      fileChanged = await runCommandWithCheck('sh', ['-c', `sudo chown ${fileData.args.owner} ${fileData.args.path}/${fileData.args.name}`], await runShellCommand('sh', ['-c', `stat -c '%U' ${fileData.args.name}`], false) == fileData.args.owner)
+    }
+    if (fileData.args.group != undefined) {
+      fileChanged = await runCommandWithCheck('sh', ['-c', `sudo chgrp ${fileData.args.group} ${fileData.args.path}/${fileData.args.name}`], await runShellCommand('sh', ['-c', `stat -c '%G' ${fileData.args.name}`], false) == fileData.args.group)
+    }
+    if (fileData.args.perms != undefined) {
+      fileChanged = await runCommandWithCheck('sh', ['-c', `sudo chmod ${fileData.args.perms} ${fileData.args.path}/${fileData.args.name}`], fileExists.includes(fileData.args.name))
+    }
   }
-  if (fileData.args.owner != undefined) {
-    await runCommandWithCheck(`sudo chown ${fileData.args.owner} ${fileData.args.name}`, `stat -c '%U' ${fileData.args.name}`, fileData.args.owner, fileData.restart)
-  }
-  if (fileData.args.group != undefined) {
-    await runCommandWithCheck(`sudo chgrp ${fileData.args.group} ${fileData.args.name}`, `stat -c '%G' ${fileData.args.name}`, fileData.args.group, fileData.restart)
-  }
-  if (fileData.args.perms != undefined) {
-    await runShellCommand(`sudo chmod ${fileData.args.perms} ${fileData.args.name}`)
+  if (fileChanged) {
+    if (fileData.restart) {
+      await handleRestartOnTask(fileData.restart.service)
+    }
+  } else {
+    console.log('***Changeset matches current configuration, skipped...***')
   }
 }
 
 async function parseServiceOptions (serviceData: ServiceTask): Promise<void> {
-  console.log(`Executing task ${serviceData.name}...`)
+  console.log(`***Executing task ${serviceData.name}...***`)
   switch(serviceData.command){
     case 'start': {
-      await runCommandWithCheck(`sudo systemctl start ${serviceData.args.service}`, `systemctl is-enabled ${serviceData.args.service}`, 'disabled', undefined)
+      await runCommandWithCheck('sh', ['-c', `sudo systemctl start ${serviceData.args.service}`], await runShellCommand('sh', ['-c', `systemctl is-enabled ${serviceData.args.service}`], false) == 'disabled')
       break
     }
     case 'restart': {
-      await runCommandWithCheck(`sudo systemctl restart ${serviceData.args.service}`, `systemctl is-enabled ${serviceData.args.service}`, 'enabled', undefined)
+      await runCommandWithCheck('sh', ['-c', `sudo systemctl restart ${serviceData.args.service}`], await runShellCommand('sh', ['-c', `systemctl is-enabled ${serviceData.args.service}`], false) == 'enabled')
       break
     }
     case 'stop': {
-      await runCommandWithCheck(`sudo systemctl stop ${serviceData.args.service}`, `systemctl is-enabled ${serviceData.args.service}`, 'enabled', undefined)
+      await runCommandWithCheck('sh', ['-c', `sudo systemctl stop ${serviceData.args.service}`], await runShellCommand('sh', ['-c', `systemctl is-enabled ${serviceData.args.service}`], false) == 'enabled')
       break
     }
     default: {
@@ -194,22 +209,40 @@ async function parseServiceOptions (serviceData: ServiceTask): Promise<void> {
 }
 
 async function handleRestartOnTask (service: string): Promise<void> {
-  console.log(`Restarting service ${service}...`)
+  console.log(`***Restarting service ${service}...***`)
   let restartCommand = `sudo systemctl restart ${service}`
   try {
-    await runShellCommand(restartCommand)
+    await runShellCommand('sh', ['-c', restartCommand], true)
   } catch (error) {
     throw new Error(`Server restart failed: ${error}`)
   }
 }
 
-async function runShellCommand (shellCommand: string): Promise<string> {
+async function runShellCommand (shellCommand: string, commandArgs: string[], stream: boolean): Promise<string> {
   return new Promise<string>((resolve, reject) => {
-    exec(shellCommand, {encoding: "utf8"}, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
+    let shell = spawn(shellCommand, commandArgs)
+    let output: ''
+    if(stream) {
+      shell.stdout.on('data', function(data){         
+        console.log(data.toString())
+      })
+    }
+    shell.stdout.on('data', function(data){         
+      output += data.toString()
+    })
+    if (shell.stderr !== null) {
+      shell.stderr.on('data', (data) => {
+        console.log(data.toString());
+      });
+    }
+    shell.on("error", function (error) {
+      reject(error)
+    })
+    shell.on("exit", function(code) {
+      if (code !== 0) {
+        reject(`Shell command failed with code ${code}`)
       } else {
-        resolve(stdout);
+        resolve(output)
       }
     })
   })
